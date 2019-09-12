5e29fb45333a8b7d99b545bf69061fb8eb65c9cf
tshimanga <oscartshimanga243@gmail.com>
2019-09-12 07:44:45 +0100

    traitement du fichier voir

---
+++ views/voir.php
@@ -0,0 +1,44 @@
+<?php include('../config/connection.php');
+    $get = $pdo->query('SELECT * FROM student');
+?>
+<?php include('../includes/header.php');?>
+<div class="row">
+    <div class="col-md-offset-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h4>List of student</h4>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <td>NOM</td>
+                            <td>POSTNOM</td>
+                            <td>PROMOTION</td>
+                            <td>UNIVERSITE</td>
+                            <td>ACTION</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       <?php while($student = $get->fetch()){ ?>
+                        <tr>
+                            <td><?=$student['nom'];?></td>
+                            <td><?=$student['postnom'];?></td>
+                            <td><?=$student['promotion'];?></td>
+                            <td><?=$student['universite'];?></td>
+                            <td>
+                                <a href="delete.php?id=<?=$student['id'];?>" class="btn btn-primary">DELETE</a>
+                                <a href="update.php?id=<?=$student['id'];?>" class="btn btn-primary">UPDATE</a>
+                            </td>
+                        </tr>
+                       <?php ;}?>
+                    </tbody>
+                </table>
+                 <div>
+                   <a class="btn btn-primary" href="../index.php">Ajouter</a>
+                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('../includes/footer.php');?>
